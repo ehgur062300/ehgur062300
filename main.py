7a659db156dac78baa02a943f9d001d3bc6a4803
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2024-01-09 12:22:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -86,6 +86,6 @@ preREADME = """
 
 resultREADME = f"{preREADME}{markdown_text}"
 
-f = open("README.md", mode="w", encoding="utf-8")
-f.write(resultREADME)
+with open("README.md", "w", encoding='utf-8') as f :
+  f.write(resultREADME)
 f.close()
