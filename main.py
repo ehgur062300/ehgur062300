6202a3b510ea61a302bfc57d037b66705239788a
Dohyuk Choi <79880998+ehgur062300@users.noreply.github.com>
2024-01-09 12:09:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 ## update_blogPost.py
 import feedparser
 
-blog_url = "https://backend-repository.tistory.com/feed.xml"
+blog_url = "https://backend-repository.tistory.com/"
 rss_feed = feedparser.parse(blog_url)
 
 MAX_NUM = 5
